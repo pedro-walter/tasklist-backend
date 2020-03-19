bd584c4ea334398d3bb533bb1d83782c04c14524
Pedro <pedrohardware@gmail.com>
2020-03-19 11:01:39 -0300

    first

---
+++ task/views.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets
+
+from .models import Task
+from .serializers import TaskSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
